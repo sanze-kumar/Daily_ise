d88bc789a01f39349dc9e3a54abfce84f4a2bc9a
sanze-kumar <125588097+sanze-kumar@users.noreply.github.com>
2023-02-16 12:51:58 -0500

    Create duplicates.py

---
+++ duplicates.py
@@ -0,0 +1,8 @@
+# If you want to create a multiple file or duplicates of file. These two line code work for you.
+import shutil
+
+# Change the number in bracket. Number of copies you wants.
+for i in range (15):
+
+# Change the working directory (XXX) and file_name with the file format (.pdb).
+shutil.copy2 ('xxx/file_name.pdb', 'xxx/file_name{}.pdb'.format(i))
